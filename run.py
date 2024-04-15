bad8dab759101a8edd2f9726494777bd01ee6917
张嘉睿 <68186626+zhangjiarui530@users.noreply.github.com>
2024-04-15 16:57:02 +0800

    4.15

--- run.py
+++ run.py
@@ -9,18 +9,14 @@ def run_command_new_window(cmd):
 
 def main():
     # 设置工作目录
-    base_dir = r"C:\开放原子\4.17"
+    base_dir = r"C:\OpenEduECNU"
 
     # 问答/出题
-    os.chdir(os.path.join(base_dir, "class_assistant\\data"))
-    run_command_new_window("chroma run --path db")
     os.chdir(os.path.join(base_dir, "class_assistant\\chat_question"))
     run_command_new_window("streamlit run OpenEduECNU1.py")
     # 大纲
     os.chdir(os.path.join(base_dir, "class_assistant\\courseware"))
     run_command_new_window("streamlit run OpenEduECNU2.py")
-    # 代码
-    # run_command_new_window("streamlit run codechat3.py")
 
     # 恢复到原始工作目录
     os.chdir(base_dir)
